1fca785f592971ccd4cf38c2df847ebe671f48d7
albalopes <alba.lopes@ifrn.edu.br>
2024-07-29 22:42:16 +0000

    Alterações no formulário de login

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'sua-palavra-secreta'
 
 @app.route('/')
 def index():
