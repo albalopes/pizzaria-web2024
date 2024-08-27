06550b05b1dcdb2ac4cb33713622a45c5f0194ce
albalopes <alba.lopes@ifrn.edu.br>
2024-08-27 00:49:05 +0000

    Ações Recovery e Create

--- controllers/diario.py
+++ controllers/diario.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, redirect, flash
+from flask import render_template, request, redirect, flash, url_for
 from database import db
 from models.diario import Diario
 from flask import Blueprint
@@ -7,4 +7,19 @@ bp_diario = Blueprint("diario", __name__, template_folder='templates')
 
 @bp_diario.route('/recovery')
 def recovery():
-    return render_template('diario_recovery.html')
+    dados = Diario.query.all()
+    return render_template('diario_recovery.html', dados=dados)
+
+@bp_diario.route('/create', methods=['GET', 'POST'])
+def create():
+    if request.method=="GET":
+        return render_template('diario_create.html')
+
+    if request.method=="POST":
+        titulo = request.form['titulo']
+        disciplina = request.form['disciplina']
+        d = Diario(titulo, disciplina)
+        db.session.add(d)
+        db.session.commit()
+        return redirect(url_for('diario.recovery'))
+    
